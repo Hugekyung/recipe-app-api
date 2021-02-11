1051a7f14f0487c429b992d78e6e0c64f3bb99a3
yang <kiki9510@gmail.com>
2021-02-12 01:01:34 +0900

    Added custom user model.

---
+++ app/core/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # 이메일 정보와 지정하는 다른 필드를 user에 넣는다
+        user.set_password(password)
+        user.save(using=self._db)  # 여러 database를 지원하기 위한 것
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model support using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
